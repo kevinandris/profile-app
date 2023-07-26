71fb3d7b75bac119c6be0f4de54f0177b45d9d68
keipy27 <kevinandris27@gmail.com>
2023-07-27 11:51:16 +1200

    react icons dependency and icons added in the App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.module.css';
 import ProfileList from './components/profile/ProfileList';
+import { AiFillGithub } from "react-icons/ai"
+import { FaGithubSquare} from "react-icons/fa"
 
 function App() {
   return (
     <div className="App">
       <ProfileList/>
+      <AiFillGithub/>
+      <FaGithubSquare />
     </div>
   );
 }
