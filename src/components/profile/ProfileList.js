fb9b8a548b8d747e3f3562b82ef367914538c239
keipy27 <kevinandris27@gmail.com>
2023-07-27 12:07:01 +1200

    initial profileList.js code implemented and its css code added as well

--- src/components/profile/ProfileList.js
+++ src/components/profile/ProfileList.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import styles from "./ProfileList.module.css"
+import Profile from './Profile'
 
 const ProfileList = () => {
   return (
-    <div>
-        <h1>Profile List</h1>
-    </div>
+    <section className={styles.center}>
+        <div>
+          <h1>Team Members</h1>
+          
+          <div className={styles["profile-container"]}>
+            <Profile/>
+          </div>
+        </div>
+    </section>
   )
 }
 
